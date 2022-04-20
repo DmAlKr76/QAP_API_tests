5a9b6c0600c626e23c4c9a8b7674087e837f0bc1
DmAlKr76 <88664355+DmAlKr76@users.noreply.github.com>
2022-04-20 15:38:05 +0300

    git add

---
+++ settings.py
@@ -0,0 +1,6 @@
+valid_email = "dka@mail.com"
+valid_password = "DKA"
+
+not_valid_email = "skdhfilagflygdfiydsgfliudgfladsigfsdagy@mail.com"
+not_valid_password = "uasdhglkuadshuidshgdghdfughofduhgudfhsg"
+not_valid_key = "lkuhkhglhfhgfldsghlfdghsfgidfghhg"
